23c93cf73b5166128e1dc573505afb6ef74020c8
rumahsahabatabk <yuniwijayanti46@guru.sd.belajar.id>
2026-08-17 23:39:29 +0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxawE-eCdnOVNvfII5yOdRwhrk0trAjBfE-FkGTChx-86yqwFUXj7L8S_YD55FPkdVU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw_xVtLqmu5ApC7_0B2CSLaNzcePmbPsF3eiqV5obvPAvxor3ar0d9jBAVwKSFgJqC6/exec";
